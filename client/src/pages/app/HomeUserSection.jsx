880324fac26b1913f8e6b3430fe10b7f0e58a424
mamamind47 <badeesorn.sit@gmail.com>
2025-04-20 22:04:45 +0700

    ver 0.4

--- client/src/pages/app/HomeUserSection.jsx
+++ client/src/pages/app/HomeUserSection.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import apiClient from "../../api/axiosConfig";
 import { CheckCircle, XCircle } from "lucide-react";
-import bgImage from "../../assets/HomeSection.png";
+import bgImage from "../../assets/HomeSection.webp";
 
 function UserHomeSection() {
   const [latestYear, setLatestYear] = useState("");
